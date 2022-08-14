96c2290ae349af0fde9ca81046c1e547a2c932c8
alejo0998 <alejobusso09@gmail.com>
2022-08-14 03:25:30 -0300

    Config

--- lsa_backend/urls.py
+++ lsa_backend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.urls import include, path
 from django.urls import path
 from rest_framework import routers, serializers, viewsets
@@ -22,7 +23,8 @@ from rest_framework.authtoken import views
 from login.views import *
 from rest_framework.permissions import IsAuthenticated
 from aprendizaje.viewset import SenaViewset, UsuarioSenaViewset
-
+from django.conf import settings
+from django.conf.urls.static import static
 # Serializers define the API representation.
 
 class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
@@ -56,4 +58,4 @@ urlpatterns = [
     path('senas/get_senas_categoria', SenaViewset.get_senas_categoria),
     path('usuario/get_senas_usuario', UsuarioSenaViewset.get_senas_usuario),
     path('usuario/post_sena_usuario', UsuarioSenaViewset.post_sena_usuario),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
