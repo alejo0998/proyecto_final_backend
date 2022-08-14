8e81542f64d38eeee59d1ee4977ff37c084c98bd
alejo0998 <alejobusso09@gmail.com>
2022-08-14 04:27:14 -0300

    ASD

--- lsa_backend/settings.py
+++ lsa_backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -149,4 +149,4 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "login.Usuario" 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
