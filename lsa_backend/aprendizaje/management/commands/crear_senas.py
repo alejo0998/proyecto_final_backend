1a88d8b46b4cbcf2ed0ef4c81bea19704fcacbdc
alejo0998 <alejobusso09@gmail.com>
2022-08-20 02:39:29 -0300

    Agrego comando para las urls

--- lsa_backend/aprendizaje/management/commands/crear_senas.py
+++ lsa_backend/aprendizaje/management/commands/crear_senas.py
@@ -1,6 +1,19 @@
+from random import gauss
 from django.core.management.base import BaseCommand
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 
-class Comando(BaseCommand):
+class Command(BaseCommand):
     def handle(self, *args, **options):
-        import pdb; pdb.set_trace()
-        return super().handle(*args, **options)
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()
+        gauth.Authorize()
+        gdrive = GoogleDrive(gauth)
+        archivos = gdrive.ListFile().GetList()
+        cantidad_archivos = 0
+        for archivo in archivos:
+            print('Nombre {}'.format(archivo['title']))
+            print('Link archivo {}'.format(archivo['embedLink']))
+            cantidad_archivos += 1
+
+        print(cantidad_archivos)
