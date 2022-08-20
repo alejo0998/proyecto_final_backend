02b17d531ac2e4353209665b3d4ef84135d81fcf
alejo0998 <alejobusso09@gmail.com>
2022-08-20 03:09:22 -0300

    Comando funcionando y crea el csv

--- lsa_backend/aprendizaje/management/commands/crear_senas.py
+++ lsa_backend/aprendizaje/management/commands/crear_senas.py
@@ -2,6 +2,7 @@ from random import gauss
 from django.core.management.base import BaseCommand
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+import csv
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -11,9 +12,20 @@ class Command(BaseCommand):
         gdrive = GoogleDrive(gauth)
         archivos = gdrive.ListFile().GetList()
         cantidad_archivos = 0
-        for archivo in archivos:
-            print('Nombre {}'.format(archivo['title']))
-            print('Link archivo {}'.format(archivo['embedLink']))
-            cantidad_archivos += 1
+        with open('/home/aprendizaje_url.csv', 'w', newline='') as csvfile:
+            fieldnames = ['Categoria', 'Nombre', 'URL']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for archivo in archivos:
+                nombre = archivo['title'].split('-')
+                if archivo['title'].endswith('.mp4') and len(nombre)>1:
+                    writer.writerow({
+                        'Categoria': nombre[0],
+                        'Nombre': nombre[1].split('.')[0],
+                        'URL': archivo['embedLink']
+                    })
+                    print('Nombre {}'.format(archivo['title']))
+                    print('Link archivo {}'.format(archivo['embedLink']))
+                    cantidad_archivos += 1
 
-        print(cantidad_archivos)
+            print(cantidad_archivos)
