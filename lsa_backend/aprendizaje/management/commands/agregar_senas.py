e0356b675324725b068b274bb697c0a4ba2304cb
alejo0998 <alejobusso09@gmail.com>
2022-08-24 01:25:03 -0300

    Agrego sena

---
+++ lsa_backend/aprendizaje/management/commands/agregar_senas.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+import csv
+from aprendizaje.models import Sena
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open('/home/aprendizaje_url.csv', 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                categoria = row['Categoria']
+                nombre = row['Nombre']
+                url = row['URL']
+                if len(categoria)<=2:
+                    Sena.objects.create(
+                        categoria = categoria,
+                        nombre = nombre,
+                        url = url
+                    )
