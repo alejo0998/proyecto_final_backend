10b8879c52446d0afcaf423c0314bb3bc2b84d22
alejo0998 <alejobusso09@gmail.com>
2022-08-24 01:38:54 -0300

    Modifico

--- aprendizaje/management/commands/agregar_senas.py
+++ aprendizaje/management/commands/agregar_senas.py
@@ -1,3 +1,4 @@
+from ast import operator
 from django.core.management.base import BaseCommand
 import csv
 from aprendizaje.models import Sena
@@ -6,9 +7,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('aprendizaje_url.csv', 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader:
+            for row in sorted(reader, reverse=False, key='nombre'):
                 categoria = row['Categoria']
-                nombre = row['Nombre']
+                nombre = row['Nombre'].replace('_', ' ')
                 url = row['URL']
                 if len(categoria)<=2:
                     Sena.objects.create(
