3b8978da28e307801b1103e5f5d29c5dd96a10cc
alejo0998 <alejobusso09@gmail.com>
2022-10-07 13:18:08 -0300

    Prueba

--- juegos/helper_viewset.py
+++ juegos/helper_viewset.py
@@ -1,16 +1,20 @@
 from re import I
+from unittest import result
 from aprendizaje.models import Sena
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
+import threading
+import time
+import gc
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
-IMAGE_HEIGHT , IMAGE_WIDTH = 1280, 720 
+IMAGE_HEIGHT , IMAGE_WIDTH = 320, 180 
 SEQUENCE_LENGTH = 30        #ACÁ DECÍA 20, LE SUBÍ A 150 PORQUE LA MAYORÍA LOS FILMAMOS A 30 FPS Y DURAN COMO MUCHO 5 SEG
 DATASET_DIR = '../media' 
-
+resultados = []
 
 class Video():
 
@@ -43,12 +47,19 @@ def mediapipe_detection(image, model):
 #    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
 #    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
 
-def extract_keypoints(results):
+def extract_keypoints(results, indice):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else zero(33*4,"pose")
     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else zero(468*3,"cara")
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else zero(21*3,"mano izq")
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else zero(21*3,"mano der")
-    return np.concatenate([pose, face, lh, rh])
+    global resultados
+    resultados.append((np.concatenate([pose, face, lh, rh]), indice))
+    del pose
+    del face
+    del lh
+    del rh
+    gc.collect()
+
 
 def zero(i,t):
   print("no encontro " + str(t))
@@ -74,60 +85,59 @@ def frames_extraction(video_memory, categoria):
     
     # Read the Video File using the VideoCapture object.
     video_reader = cv2.VideoCapture(video_memory.file.name)
+    del video_memory
+    gc.collect()
 
     # Get the total number of frames in the video.
     video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
 
+
     # Calculate the the interval after which frames will be added to the list.
     skip_frames_window = max(int(video_frames_count/SEQUENCE_LENGTH), 1)
-
     # Iterate through the Video Frames.
+  
     for frame_counter in range(SEQUENCE_LENGTH):
-
-        # Set the current frame position of the video.
-        video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_counter * skip_frames_window)
-
-        # Reading the frame from the video. 
-        success, frame = video_reader.read() 
-
-        # Check if Video frame is not successfully read then break the loop
-        if not success:
-            break
-
-        # Resize the Frame to fixed height and width.
-        resized_frame = cv2.resize(frame, (IMAGE_WIDTH, IMAGE_HEIGHT))
-        #resized_frame = cv2.cvtColor(resized_frame,cv2.COLOR_BGR2RGB)
-        #cv2_imshow(frame)
-        # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1
-        #normalized_frame = resized_frame / 255
-
-        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-          # Make detections
-          image, result = mediapipe_detection(resized_frame, holistic)
-          
-          # Draw landmarks
-          #draw_landmarks(image, result)
-          results = extract_keypoints(result)
-          video_keypoints.append(results)
-          """print("mano izq")
-          print(result)
-          if(result.left_hand_landmarks):
-            print(len(result.left_hand_landmarks.landmark))"""
-          # Show to screen
-          #cv2_imshow(image)
-        
-        # Append the normalized frame into the frames list
-        #frames_list.append(image)
-        
+      #t = threading.Thread(target=magia_2, args=(video_reader, skip_frames_window, frame_counter,  ))
+      #t.start()
+      magia(video_reader, skip_frames_window, frame_counter)
+      
+      #magia_2(video_reader, skip_frames_window, frame_counter)
+    
+  
     # Release the VideoCapture object. 
     video_reader.release()
     #video.frames = frames_list
+    global resultados
+    resultados.sort(key=lambda x:x[1])
+    for resultado in resultados:
+      video_keypoints.append(resultado[0])
     video.keypoints = video_keypoints
-
-    
-    # Return the frames list.
+    del resultados
     return video
     #return frames_list, video_keypoints
+
+def magia(video_reader, skip_frames_window, frame_counter):
+    video_reader.set(cv2.CAP_PROP_POS_FRAMES, frame_counter * skip_frames_window)
+    success, frame = video_reader.read()
+
+    if not success:
+        return "Error"
+
+    resized_frame = cv2.resize(frame, (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+      image, result = mediapipe_detection(resized_frame, holistic)
+      t = threading.Thread(target=extract_keypoints, args=(result, frame_counter,))
+      #extract_keypoints(result, frame_counter)
+      t.run()
+      del t
+      del image
+      del resized_frame
+      del result
+      del holistic
+    gc.collect()
+
+
 
 def predict(video, categoria):
     categoria = categoria
@@ -138,9 +148,15 @@ def predict(video, categoria):
     video = frames_extraction(video, categoria)
     print("Extraje los frames")
     test_keypoints = list(video.keypoints)
+    del video
+    gc.collect()
     list_test = list()
     list_test.append(test_keypoints)
-    predictions = model.predict((np.array(list_test)))
+    lista = np.array(list_test)
+    predictions = model.predict(lista)
+    del list_test
+    del lista
+    gc.collect()
     print("Ya predije")
     posibles_senas = Sena.objects.filter(categoria=categoria)[0:4]
     if len(posibles_senas) != len(predictions[0]):
