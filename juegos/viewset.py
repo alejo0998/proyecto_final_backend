4f4e3e5b25eb2c183e4f0cc4618394b43da52c68
alejo0998 <alejobusso09@gmail.com>
2022-10-11 19:59:37 -0300

    modifico signa

--- juegos/viewset.py
+++ juegos/viewset.py
@@ -101,7 +101,7 @@ class JuegoViewset(viewsets.ModelViewSet):
     @api_view(['GET'])
     def get_juegos_v2(request):
         categoria = request.GET.get('categoryName')
-        posibles_juegos = ['escribi', 'adivina', 'signa']
+        posibles_juegos = ['escribi', 'adivina']
         response = []
         if request._auth and categoria:
             int_categoria = obtener_categoria(categoria)
@@ -117,34 +117,13 @@ class JuegoViewset(viewsets.ModelViewSet):
                                 'name': sena.nombre,
                                 'urlVideo': sena.url
                 }
-                tipo_juego = random.choice(posibles_juegos)
-                if tipo_juego == 'adivina':
-                    senas_opciones = list(Sena.objects.filter(categoria=int_categoria))
-                    senas_options = []
-                    for int in range(4):
-                        sena_random = random.choice(senas_opciones)
-                        senas_opciones.remove(sena_random)
-                        senas_options.append(sena_random)
-                    if not(sena in senas_options):
-                        senas_options.pop(0)
-                        senas_options.insert(random.randint(0,3),sena)
-                    opcion_sena = []
-                    for opcion in senas_options:
-                        op = {
-                            'text': opcion.nombre,
-                            'correct': opcion.nombre==sena.nombre
-                        }
-                        opcion_sena.append(op)
+                if sena.permite_signa:
                     respuesta = {
-                            'name': 'Adiviná la seña',
-                            'type': 'GUESS_THE_SIGN',
-                            'sign':sena_response,
-                            'options': opcion_sena                        
-                    }
-                elif tipo_juego == 'escribi':
-                    respuesta = {
-                            'name': 'Escribi la seña',
-                            'type': 'WRITE_THE_SIGN',
+                            'name': 'Signá la palabra',
+                            'description':'Juego de signa la palabra',
+                            'type': 'SIGN_THE_WORD',
+                            'position': sena.posicion_array_ia,
+                            'category': sena.categoria,
                             'sign':sena_response,
                             'options':[
                                 {
@@ -153,16 +132,43 @@ class JuegoViewset(viewsets.ModelViewSet):
                                 }
                             ]
                     }
-                elif tipo_juego == 'signa':
-                    respuesta = {
-                            'name': 'Signa la seña',
-                            'type': 'SIGN',
-                            'sign':sena_response,
-                    }
+                else:
+                    tipo_juego = random.choice(posibles_juegos)
+                    if tipo_juego == 'adivina':
+                        senas_opciones = list(Sena.objects.filter(categoria=int_categoria))
+                        senas_options = []
+                        for int in range(4):
+                            sena_random = random.choice(senas_opciones)
+                            senas_opciones.remove(sena_random)
+                            senas_options.append(sena_random)
+                        if not(sena in senas_options):
+                            senas_options.pop(0)
+                            senas_options.insert(random.randint(0,3),sena)
+                        opcion_sena = []
+                        for opcion in senas_options:
+                            op = {
+                                'text': opcion.nombre,
+                                'correct': opcion.nombre==sena.nombre
+                            }
+                            opcion_sena.append(op)
+                        respuesta = {
+                                'name': 'Adiviná la seña',
+                                'type': 'GUESS_THE_SIGN',
+                                'sign':sena_response,
+                                'options': opcion_sena                        
+                        }
+                    elif tipo_juego == 'escribi':
+                        respuesta = {
+                                'name': 'Escribi la seña',
+                                'type': 'WRITE_THE_SIGN',
+                                'sign':sena_response,
+                                'options':[
+                                    {
+                                        'text':sena.nombre,
+                                        'correct':True
+                                    }
+                                ]
+                        }
                 response.append(respuesta)
             return Response(response, 201)
         return Response('No envio token de inicio de sesion o la categoria', 400)
-
-    def juego_signar(request):
-        
-        return Response(True, 201)
