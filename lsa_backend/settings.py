8c2b01e4e97d93eac09a0c8aa5cc6cdf23307667
alejo0998 <alejobusso09@gmail.com>
2022-10-11 19:35:22 -0300

    Cambio

--- lsa_backend/settings.py
+++ lsa_backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'lsa_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if False:
+if True:
     DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
